fe83fe00e0fab7d63da07ec8a5825abdbc9ec57c
rivaldofez <rivaldofez@gmail.com>
2020-12-28 22:48:51 +0700

    create item food

--- lib/src/homescreen.dart
+++ lib/src/homescreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:foody/src/widgets/food_category.dart';
 import 'package:foody/src/widgets/home_top_info.dart';
 import 'package:foody/src/widgets/search_field.dart';
+import 'package:foody/src/widgets/bought_foods.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -22,6 +23,38 @@ class _HomeScreenState extends State<HomeScreen> {
             height: 20.0,
           ),
           SearchField(),
+          SizedBox(
+            height: 20.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Frequently Bought Food",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  "View All",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.orangeAccent,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            child: BoughtFoods(),
+          ),
         ],
       ),
     );
---
+++ lib/src/widgets/bought_foods.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class BoughtFoods extends StatefulWidget {
+  @override
+  _BoughtFoodsState createState() => _BoughtFoodsState();
+}
+
+class _BoughtFoodsState extends State<BoughtFoods> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: Stack(
+        children: [
+          Container(
+            height: 200.0,
+            width: 340.0,
+            child: Image.asset(
+              "assets/images/breakfast.jpeg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            left: 0.0,
+            bottom: 0.0,
+            child: Container(
+              height: 60.0,
+              width: 340.0,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                colors: [Colors.black, Colors.black12],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+              )),
+            ),
+          ),
+          Positioned(
+            left: 10.0,
+            bottom: 10.0,
+            right: 10.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hot Coffee",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Theme.of(context).primaryColor,
+                          size: 16.0,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Theme.of(context).primaryColor,
+                          size: 16.0,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Theme.of(context).primaryColor,
+                          size: 16.0,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Theme.of(context).primaryColor,
+                          size: 16.0,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Theme.of(context).primaryColor,
+                          size: 16.0,
+                        ),
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                        Text(
+                          "(22.0 Reviews)",
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "22",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orangeAccent,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    Text(
+                      "Min Order",
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
